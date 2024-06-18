d1eaff0c25d5a3d9d0f3d0ae94dcec7ee6c34b34
Hugo Colman <hugocolman997@gmail.com>
2024-06-18 11:28:53 -0400

    feat challenge 6

--- conversorTemperatura.py
+++ conversorTemperatura.py
@@ -1,6 +1,5 @@
 #conversor temperatura de celsius a fahrenheit
 C = float(input("Ingresa el valor de la temperatura en grados Celsius: "))
 f = (C*9/5)+32 #formula para la conversion de celsuis a fahrenheit
-
 print("La temperatura en fahrenheit es: ", f)
 
