4d3cc5b8a78a09c26c19526fd0157e43f46bbd30
Hugo Colman <136193338+huggco97@users.noreply.github.com>
2024-06-21 12:35:52 -0400

    feat challenge 9

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout << "Ingrese el primer numero entero para realizar la suma, por favor: " << endl;
+    cin >> num1;
+
+    cout << "Ingrese el primer numero entero para realizar la suma, por favor: " << endl;
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "La suma de ambos numeros es: " << sum << endl;
+
+    return 0;
+}
+
